26d40779b331895755fa8000fa83b7ffab642f3c
MentalSep <chahidjava@hotmail.com>
2023-11-12 11:05:13 +0100

    add task 6

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,6 @@
 """ Module to create unique FileStorage instance for the application """
 from models.engine.file_storage import FileStorage
 
-
+""" Create a unique FileStorage instance for the application"""
 storage = FileStorage()
 storage.reload()
